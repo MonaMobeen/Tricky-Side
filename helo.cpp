fd342f9901f141d107a39789cb42bee5c9cb9e41
MemoonaMobeen <memoonamobeen618@gmail.com>
2022-12-31 17:08:17 +0500

    first commit

---
+++ helo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>  
+int main ()  
+{  
+// Taking two variables   
+int a = 12;  
+int b = 12;  
+if ( !( a ^ b ) )  
+    printf ( " a is equal to b" );  
+else  
+    printf ( " a does not match b " );  
+return 0;  
+}
